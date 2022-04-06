0617372451e60c7714f5cce67ab9d783d7bd360b
1924962 <1924962@01-19802.pusd.dom>
2022-04-06 14:03:00 -0700

    new stuffs

---
+++ SpaceInvaders/src/Background.java
@@ -0,0 +1,64 @@
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Background{
+	//add location attributes
+	private int x,y; //position of the bird
+	private Image img; 	
+	private AffineTransform tx;
+
+	public Background(int x, int y) {
+		img = getImage("/imgs/Background.png"); //load the image for Tree.
+		this.x = y;
+		this.y =x;
+		
+		tx = AffineTransform.getTranslateInstance(x, y);
+		init(0, 0); 				//initialize the location of the image
+									//use your variables
+		tx.scale(1.7, 2);
+	}
+	
+	public void changePicture(String newFileName) {
+		img = getImage(newFileName);
+		init(0, 0);
+	}
+	
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(img, tx, null);
+		
+		
+		
+		
+	}
+	//update the picture variable location
+	private void update() {
+		tx.setToTranslation(x, y);
+		tx.scale(.5, .5);
+	}
+	
+	
+	
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(.5, .5);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Background.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+}
--- SpaceInvaders/src/Frame.java
+++ SpaceInvaders/src/Frame.java
@@ -1,13 +1,106 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
-public class Frame extends JFrame{
-	Frame(){
-		
-		this.add(new Runner());
-		this.setTitle("Space" + "Invaders");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
-		this.pack();
-		this.setVisible(true);
-		this.setLocationRelativeTo(null);
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
+//	Duck d = new Duck();
+//	Background bg = new Background(0, 0);
+//	Ghostthing w = new Ghostthing();
+//	//Crosshair CH = new Crosshair();
+	
+	
+//	public void paint(Graphics g) {
+//		super.paintComponent(g);
+//		bg.paint(g);
+//		w.paint(g);
+//		d.paint(g);
+//		//CH.paint(g);
+//	}
+//	
+	public static void main(String[] arg) {
+		Frame f = new Frame();
 	}
+	
+	public Frame() {
+		JFrame f = new JFrame("Duck Hunt");
+		f.setSize(new Dimension(900, 600));
+		f.setBackground(Color.blue);
+		f.add(this);
+		f.setResizable(false);
+		f.setLayout(new GridLayout(1,2));
+		f.addMouseListener(this);
+		f.addKeyListener(this);
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+	
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println(arg0.getKeyCode());
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
