11c92f3341302406c774dce88a3d62445bda3b50
1914067 <1914067@01-19820.pusd.dom>
2022-04-08 14:17:45 -0700

    ff

--- SpaceInvaders/src/Background.java
+++ SpaceInvaders/src/Background.java
@@ -1,3 +1,4 @@
+
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -13,7 +14,7 @@ public class Background{
 	private AffineTransform tx;
 
 	public Background(int x, int y) {
-		img = getImage("/imgs/black.png"); //load the image for Tree.
+		img = getImage("/imgs/Background.png"); //load the image for Tree.
 		this.x = y;
 		this.y =x;
 		
